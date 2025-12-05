b2f01f60d6e954bc5a68beb03f24092a8f1d9c5f
clclhive <jwk728@naver.com>
2025-12-05 12:46:36 +0900

    test

--- src/main/java/com/koreait/www/config/RootConfig.java
+++ src/main/java/com/koreait/www/config/RootConfig.java
@@ -2,9 +2,13 @@ package com.koreait.www.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -19,18 +23,34 @@ public class RootConfig {
 	public DataSource dataSource() {
 		HikariConfig hikariConfig = new HikariConfig();
 		
-		// 드라이버 클래스 이름 (Log4jdbc 용)
+		// Log4jdbc를 사용하므로 드라이버와 URL이 log4jdbc 전용으로 설정됨 (정상)
 		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
 		
-		// URL (log4jdbc 포함)
-		// 주의: "schema_name" 부분을 본인이 만든 DB 이름(예: test_db)으로 꼭 바꾸세요!
-		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3306/schema_name?serverTimezone=Asia/Seoul");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3306/springdb");
 		
-		hikariConfig.setUsername("root");
-		hikariConfig.setPassword("1234");
+		hikariConfig.setUsername("springuser");
+		hikariConfig.setPassword("mysql");
 		
-		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-		return dataSource;
+		// HikariCP 성능 최적화 설정 (선택사항, 필요시 추가)
+		// hikariConfig.setMaximumPoolSize(10);
+		
+		return new HikariDataSource(hikariConfig);
+	}
+	
+	// [추가됨] MyBatis가 DB에 접속하고 SQL을 실행할 수 있게 해주는 공장 객체
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource()); // 위에서 만든 dataSource 주입
+		return sessionFactory.getObject();
+	}
+	
+	// [추가됨] @EnableTransactionManagement를 위해 필요한 트랜잭션 관리자
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		DataSourceTransactionManager tm = new DataSourceTransactionManager();
+		tm.setDataSource(dataSource());
+		return tm;
 	}
 
 }
