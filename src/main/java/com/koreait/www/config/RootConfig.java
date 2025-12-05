77bf37cb5d7657511568ac128f49879d4870b2b7
clclhive <jwk728@naver.com>
2025-12-05 12:51:05 +0900

    test2

--- src/main/java/com/koreait/www/config/RootConfig.java
+++ src/main/java/com/koreait/www/config/RootConfig.java
@@ -5,8 +5,10 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -17,35 +19,42 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 @EnableTransactionManagement
 @MapperScan(basePackages = "com.koreait.www.repository")
+@PropertySource("classpath:database.properties") // 1. 프로퍼티 파일 로딩
 public class RootConfig {
+
+	// 2. @Value로 값 주입 (파일에 있는 키 이름과 맞춰야 함)
+	@Value("${db.driver}")
+	private String driver;
+	
+	@Value("${db.url}")
+	private String url;
+	
+	@Value("${db.username}")
+	private String username;
+	
+	@Value("${db.password}")
+	private String password;
 
 	@Bean
 	public DataSource dataSource() {
 		HikariConfig hikariConfig = new HikariConfig();
 		
-		// Log4jdbc를 사용하므로 드라이버와 URL이 log4jdbc 전용으로 설정됨 (정상)
-		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
-		
-		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3306/springdb");
-		
-		hikariConfig.setUsername("springuser");
-		hikariConfig.setPassword("mysql");
-		
-		// HikariCP 성능 최적화 설정 (선택사항, 필요시 추가)
-		// hikariConfig.setMaximumPoolSize(10);
+		// 3. 변수로 설정값 대입
+		hikariConfig.setDriverClassName(driver);
+		hikariConfig.setJdbcUrl(url);
+		hikariConfig.setUsername(username);
+		hikariConfig.setPassword(password);
 		
 		return new HikariDataSource(hikariConfig);
 	}
 	
-	// [추가됨] MyBatis가 DB에 접속하고 SQL을 실행할 수 있게 해주는 공장 객체
 	@Bean
 	public SqlSessionFactory sqlSessionFactory() throws Exception {
 		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource()); // 위에서 만든 dataSource 주입
+		sessionFactory.setDataSource(dataSource());
 		return sessionFactory.getObject();
 	}
 	
-	// [추가됨] @EnableTransactionManagement를 위해 필요한 트랜잭션 관리자
 	@Bean
 	public PlatformTransactionManager transactionManager() {
 		DataSourceTransactionManager tm = new DataSourceTransactionManager();
